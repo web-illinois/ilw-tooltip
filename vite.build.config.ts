729f5e63b15fcc41bc19fe35b3d56d0ce69615da
Natalie Johnstone <nhj@illinois.edu>
2025-09-30 10:18:18 -0500

    converted to typescript

---
+++ vite.build.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    root: "src",
+    build: {
+        outDir: "../dist/cdn",
+        lib: {
+            name: "ilw-tooltip",
+            entry: "ilw-tooltip.ts",
+            fileName: "ilw-tooltip",
+            formats: ["es"],
+        },
+        rollupOptions: {
+            output: {
+                assetFileNames: () => {
+                    return "[name][extname]";
+                }
+            },
+        },
+    },
+    server: {
+        hmr: false,
+    },
+});
